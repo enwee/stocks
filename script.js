327cb13713b8bf86f32dcdc3cc4d38b2b20c85d2
enwee <tanenwee@gmail.com>
2025-06-12 08:40:04 +0800

    do 52w svg, formatting, fix rates was not in local time

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const eightHrMsecs = 8 * 60 * 60 * 1000
+
 const get = key => JSON.parse(localStorage.getItem(key))
 
 const urls = get("urls")
@@ -17,7 +19,11 @@ const officeHours = () => {
   return true
 }
 
+const beforeToday8am = time => // time shifted back 8hrs to get 8am Date change
+  new Date(time - eightHrMsecs).toDateString() != new Date(Date.now() - eightHrMsecs).toDateString()
+
 const getQuotes = async () => {
+  console.log('getting quotes...')
   const resp = await fetch(useProxy(urls.quotes))
   const data = await resp.json()
   const quotes = data.data.prices.filter(quote => counters.includes(quote.nc))
@@ -25,30 +31,33 @@ const getQuotes = async () => {
       acc[quote.nc] = quote
       return acc
     }, {})
+  console.log(`quotes done (${Date(data.meta.processedTime)})`)
   return [quotes, data.meta.processedTime]
 }
 
 const getRates = async () => {
-  let rates = get("rates")
-  if (!rates || new Date(rates.time).toDateString() != new Date().toDateString()) {
+  let rates = get("rates...")
+  if (!rates || beforeToday8am(rates.time)) {
+    console.log(`getting rates...`)
     rates = {}
     const resp = await fetch(urls.rates)
     const data = await resp.json()
     for (const rate of ["SGD", "JPY", "CNY", "HKD"]) {
       rates[rate] = data.conversion_rates[rate]
     }
-    rates.time = data.time_last_update_unix * 1000
+    rates.time = data.time_last_update_unix * 1000 + eightHrMsecs // store in local time
     localStorage.setItem("rates", JSON.stringify(rates))
+    console.log(`rates done (${Date(rates.time)})`)
   }
   return rates
 }
 
 const getFinancials = async () => {
   let financials = get("financials")
-  if (!financials || // financials.time shifted back 8hrs to get 8am Date change
-    new Date(financials.time - 28800000).toDateString() != new Date(Date.now() - 28800000).toDateString()) {
+  if (!financials || beforeToday8am(financials.time)) {
     financials = {}
     for (const symbol of counters) {
+      console.log(`getting financials ${symbol}...`)
       const resp = await fetch(useProxy(urls.financials.replace("{CODE}", symbol)))
       const data = (await resp.json()).data[0] || {}
       for (key in data) {
@@ -58,8 +67,16 @@ const getFinancials = async () => {
     }
     financials.time = Date.now()
     localStorage.setItem("financials", JSON.stringify(financials))
+    console.log(`financials done (${Date(financials.time)})`)
   }
   return financials
+}
+
+const sdrInfo = ({ lt: last, nc: symbol }, rates) => {
+  const sdrs = { HBBD: { ratio: 5, currency: "HKD" } }
+  const { ratio, currency } = sdrs[symbol]
+  const v = last * ratio * rates[currency] / rates.SGD * 0.99 // apply 1% lower rate
+  return `${currency} ${v.toFixed(2)}`
 }
 
 const updateState = async () => {
@@ -84,7 +101,7 @@ const updateState = async () => {
     // what about PE/PB by last done
     Object.assign(stocks[symbol], financials[symbol])
     if (stocks[symbol].type == "adrs") {
-      stocks[symbol].sdrInfo = "..."
+      stocks[symbol].sdrInfo = sdrInfo(stocks[symbol], rates)
     }
   }
 
@@ -92,8 +109,8 @@ const updateState = async () => {
   totals.monitored.gain_loss = totals.reits.gain_loss + totals.stocks.gain_loss
 
   totals.reits.meta = `1USD = ${rates.SGD.toFixed(3)}SGD`
-  totals.stocks.meta = `1SGD = ${(rates.JPY / rates.SGD).toFixed(2)}JPY`
-  totals.monitored.meta = `10CNY = ${(rates.SGD / rates.CNY * 10).toFixed(2)}SGD`
+  totals.stocks.meta = `1SGD = ${(rates.JPY / rates.SGD).toFixed(3)}JPY`
+  totals.monitored.meta = `10CNY = ${(rates.SGD / rates.CNY * 10).toFixed(3)}SGD`
   return [stocks, time, totals]
 }
 
@@ -128,25 +145,45 @@ const data = () => {
 
 const columns = [
   {
-    label: "Company Name", alias: "n", type: "name", format: ({ n, nc }) => {
-      const words = n.split(" ")
+    label: "Company Name", alias: "n", type: "name", format: ({ n: name, nc: code }) => {
+      const words = name.split(" ")
       if (words.length > 2) {
-        n = words.slice(0, 3).join(" ")
-        n = n.length > 16 ? words.slice(0, 2).join(" ") : n
+        name = words.slice(0, 3).join(" ")
+        name = name.length > 16 ? words.slice(0, 2).join(" ") : name
       }
-      return `<div class="text-violet-400 text-left" onclick="window.open('${urls.sgx.replace('{CODE}', nc)}')">${n}</div>`
+      return `<div class="text-violet-400 text-left" 
+      onclick="window.open('${urls.sgx.replace('{CODE}', code)}')">${name}</div>`
     }
   },
+
   { label: "Last", alias: "lt", type: "watched", format: num => num },
   { label: "Change", alias: "c", type: "watched", format: num => `<div class="${color(num)}">${num}</div>` },
   { label: "%", alias: "p", type: "watched", format: num => `<div class="${color(num)}">${num.toFixed(1)}</div>` },
+
   { label: "High", alias: "h", type: "default", format: num => num },
   { label: "Low", alias: "l", type: "default", format: num => num },
-  { label: "52h", alias: "fiftyTwoWeekHigh", type: "default", format: num => num || "-" },
-  { label: "52l", alias: "fiftyTwoWeekLow", type: "default", format: num => num || "-" },
+
+  {
+    label: "52 week H/L", alias: "-", type: "52w",
+    format: ({ fiftyTwoWeekHigh, fiftyTwoWeekLow, lt: last, h: high, l: low, type, sdrInfo }) => {
+      if (type == "adrs") {
+        return `(${sdrInfo})`
+      }
+      const pixel = (fiftyTwoWeekHigh - fiftyTwoWeekLow) / 50
+      return `<div class="pr-2 w-16">${fiftyTwoWeekHigh}</div>
+      <svg class="bg-violet-900" width="50" height="10" xmlns="http://www.w3.org/2000/svg">
+        <rect class="fill-current text-violet-400" height="10" 
+          width="${(high - low) / pixel}" x="${(fiftyTwoWeekHigh - high) / pixel}" />
+        <rect class="fill-current" width="1" height="10" x="${(fiftyTwoWeekHigh - last) / pixel}" />
+      </svg>
+      <div class="text-left pl-2 w-16">${fiftyTwoWeekLow}</div>`
+    }
+  },
+
   { label: "P/E", alias: "peRatio", type: "default", format: num => num ? num.toFixed(2) : "-" },
   { label: "P/B", alias: "priceBookValue", type: "default", format: num => num ? num.toFixed(2) : "-" },
   { label: "Avg Px", alias: "avg_price", type: "default", format: num => num ? num.toFixed(2) : "-" },
+
   { label: "Mkt Val", alias: "mkt_value", type: "watched", format: num => num ? numComma(num) : "-" },
   { label: "Gain/Loss", alias: "gain_loss", type: "watched", format: num => num !== null ? numComma(num, true) : "-" },
 ]
@@ -163,3 +200,4 @@ const green = "text-emerald-500"
 const red = "text-rose-700"
 const color = num => num > 0 ? green : num < 0 ? red : ""
 const button = "bg-violet-900 hover:bg-violet-400 px-4 rounded-full"
+const blink = bool => bool ? "animate-(--animate-true)" : "animate-(--animate-false)"
