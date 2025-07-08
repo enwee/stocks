fd4ad82695b5b22799e1808ea1f9b94dec23933b
enwee <tanenwee@gmail.com>
2025-07-08 22:59:10 +0800

    forex save last inputs and styling

--- js/forex.js
+++ js/forex.js
@@ -3,39 +3,47 @@ const urls = get("urls")
 
 // referenceTime = Date.now() default is needed for when directly calling this from console
 const getRates = async (referenceTime = Date.now()) => {
-    let rates = get("rates")
-    if (!rates || referenceTime > rates.nextUpdateTime) {
-        console.log(`getting rates...`)
-        rates = { time: 0, lastUpdateTime: "", nextUpdateTime: 0 }
-        const resp = await fetch(urls.rates)
-        const data = await resp.json()
-        for (const rate of ["USD", "JPY", "CNY", "HKD", "SGD"]) {
-            rates[rate] = data.conversion_rates[rate]
-        }
-        rates.time = Date.now()
-        rates.lastUpdateTime = new Date(data.time_last_update_unix * 1000).toString()
-        rates.nextUpdateTime = data.time_next_update_unix * 1000
-        localStorage.setItem("rates", JSON.stringify(rates))
-        console.log(`rates done (${Date(rates.time)})`)
+  let rates = get("rates")
+  if (!rates || referenceTime > rates.nextUpdateTime) {
+    console.log(`getting rates...`)
+    rates = { time: 0, lastUpdateTime: "", nextUpdateTime: 0 }
+    const resp = await fetch(urls.rates)
+    const data = await resp.json()
+    for (const rate of ["USD", "JPY", "CNY", "HKD", "SGD"]) {
+      rates[rate] = data.conversion_rates[rate]
     }
-    return rates
+    rates.time = Date.now()
+    rates.lastUpdateTime = new Date(data.time_last_update_unix * 1000).toString()
+    rates.nextUpdateTime = data.time_next_update_unix * 1000
+    localStorage.setItem("rates", JSON.stringify(rates))
+    console.log(`rates done (${Date(rates.time)})`)
+  }
+  return rates
 }
 
-const exchange = async ({ val1, cur1, cur2 }) => {
-    const rates = await getRates()
-    let val2 = val1 * (1 / rates[cur1]) / (1 / rates[cur2])
-    val2 = val2 > 100 ? Math.round(val2) : Math.round(val2 * 100) / 100
-    return val2
+const exchange = async (val, fromCur, toCur) => {
+  const rates = await getRates()
+  const amt = val * (1 / rates[fromCur]) / (1 / rates[toCur])
+  return amt > 1000 ? Math.round(amt) : Math.round(amt * 100) / 100
 }
 
-const xData = () => ({
-    val1: 10,
-    cur1: "CNY",
-    val2: null,
-    cur2: "SGD",
-    async init() {
-        this.val2 = await exchange(this)
-    }
+const save = xData => {
+  // xData is from alpinejs $data and cannot do object spread.
+  const data = JSON.parse(JSON.stringify(xData)); // copy of xData
+  ["val2", "val4"].forEach(key => data[key] = null)
+  localStorage.setItem("lastForex", JSON.stringify(data))
+}
+
+
+const xData = () => get("lastForex") || ({
+  val1: 10,
+  cur1: "CNY",
+  val2: null,
+  cur2: "SGD",
+  val3: 1,
+  cur3: "SGD",
+  val4: null,
+  cur4: "JPY",
 })
 
 const flags = { CNY: "🇨🇳", JPY: "🇯🇵", SGD: "🇸🇬", USD: "🇺🇸", HKD: "🇭🇰" }
