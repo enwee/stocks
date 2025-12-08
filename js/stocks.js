928828fba9bf50c525263728b010e6a46b0d6fc3
enwee <tanenwee@gmail.com>
2025-12-09 01:26:25 +0800

    drop SEK, add TWD

--- js/stocks.js
+++ js/stocks.js
@@ -52,7 +52,7 @@ const getRates = async (referenceTime = Date.now()) => {
     rates = { time: 0, lastUpdateTime: "", nextUpdateTime: 0 }
     const resp = await fetch(urls.rates)
     const data = await resp.json()
-    for (const rate of ["USD", "JPY", "CNY", "HKD", "SGD", "IDR", "SEK", "EUR"]) {
+    for (const rate of ["USD", "JPY", "CNY", "HKD", "SGD", "IDR", "EUR", "TWD"]) {
       rates[rate] = data.conversion_rates[rate]
     }
     rates.time = new Date().toString()
